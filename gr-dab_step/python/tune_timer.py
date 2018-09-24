77154a278a502de7cf4f9e7e2c6e299d388aed06
schneider <schneider@blinkenlichts.net>
2018-09-24 21:27:08 +0200

    feat(dab-step): Let's dance

--- gr-dab_step/python/tune_timer.py
+++ gr-dab_step/python/tune_timer.py
@@ -24,6 +24,7 @@ from gnuradio import gr
 import pmt
 from monotonic_cffi import monotonic
 from threading import Timer
+import signal
 
 class tune_timer(gr.sync_block):
     """
@@ -37,9 +38,11 @@ class tune_timer(gr.sync_block):
         self.message_port_register_out(gr.pmt.intern('command'))
         self.sample_rate = 2048000
         self.last_rx_time = (None, None)
-        self.timer1 = None
+        self.last_rx_time = (0, 0)
         self.i = 0
         self.t1 = 0
+        signal.signal(signal.SIGALRM, self.handler)
+
 
     def monotonic_raw_from_offset(self, offset):
         return (self.last_rx_time[1] + (offset - self.last_rx_time[0]) * 1e6 / float(self.sample_rate))/1e6
@@ -49,64 +52,52 @@ class tune_timer(gr.sync_block):
                 pmt.list2(pmt.from_double(freq), pmt.from_uint64(0)))
         self.message_port_pub(pmt.intern('command'), cmd)
 
+    def handler(self, signum, frame):
+        self.tune_1()
+
     def tune_1(self):
         t = monotonic()
-        #self.timer1.cancel()
-        #self.timer1 == Timer(0.096, self.tune_1)
-        #self.timer1.start()
+        #self.t20 = monotonic()
+        self.timer = Timer(0.082, self.tune_2)
+        self.timer.start()
 
-        self.t20 = monotonic()
-        #self.timer2 = Timer(0.020, self.tune_2)
-        self.timer2 = Timer(0.080, self.tune_2)
-        self.timer2.start()
-
-        #self.send_tune(222064000)
         self.send_tune(100000000)
 
-        print "t1 fired after", self.t10 - t, "should have after", self.t1
+        #print "t1 fired after", self.t10 - t, "should have after", self.t1
+
     def tune_2(self):
-        t = monotonic()
-        #self.send_tune(100000000)
+        #t = monotonic()
         self.send_tune(222064000)
-        print "t2 fired after", self.t20 - t, "should have after 0.02"
+        #print "t2 fired after", self.t20 - t, "should have after 0.02"
 
     def update_timer(self, next_prs):
         self.i += 1
         if self.i < 10:
             return
-        if self.timer1:
-            # Yes, there is a race condition
-            self.timer1.cancel()
         self.t10 = monotonic()
-        #self.t1 = next_prs - self.t10 - 0.01
-        #self.t1 = next_prs - self.t10 + 0.01
-        self.t1 = next_prs - self.t10 - 0.005
-        self.timer1 = Timer(self.t1, self.tune_1)
-        #self.timer1 = Timer(next_prs - monotonic(), self.tune_1)
-        self.timer1.start()
+        self.t1 = next_prs - self.t10 - 0.006
+        #print "setting timer for", self.t1
+        signal.setitimer(signal.ITIMER_REAL, self.t1, 0.096)
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
         n = len(in0)
 
-        #nread = self.nitems_read(0)
-        #tags = self.get_tags_in_range(0, nread, nread+n)
         tags = self.get_tags_in_window(0, 0, n)
         for tag in tags:
-            print tag.offset
             key = pmt.symbol_to_string(tag.key)
             if key == 'start_prs':
                 value = pmt.to_double(tag.value)
                 prs_rx_time = self.monotonic_raw_from_offset(tag.offset)
-                print "prs @", tag.offset + value, prs_rx_time
-                print "that was", monotonic() - prs_rx_time, "seconds ago"
+                #print "prs @", tag.offset + value, prs_rx_time
+                #print "that was", monotonic() - prs_rx_time, "seconds ago"
                 next_prs = prs_rx_time + 0.096 * (int((monotonic() - prs_rx_time) / 0.096) + 1)
-                print "next prs @", next_prs
+                #print "next prs @", next_prs
                 next_prs_in = next_prs - monotonic()
-                print "next prs in", next_prs - monotonic()
-                #if next_prs_in > 0.03:
-                self.update_timer(next_prs)
+                #print "next prs in", next_prs - monotonic()
+                if next_prs_in > 0.010:
+                    self.update_timer(next_prs)
             elif key == 'rx_time':
                 value = pmt.to_uint64(tag.value)
                 #print "sample", tag.offset, "sampled at", value
